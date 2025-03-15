ff711d7a2bdf65333793d1932b63fd3c82faed43
Serge Zaugg <serge.zaugg@gmail.com>
2025-03-15 12:38:21 +0100

    so far so good, gonna have lunch

--- st_page_02.py
+++ st_page_02.py
@@ -1,25 +1,16 @@
 #--------------------             
 # Author : Serge Zaugg
-# Description : Create datasets, train, assess performance and feature importance
+# Description : some info 
 #--------------------
 
 import os
 import streamlit as st
-from streamlit import session_state as ss
-import numpy as np
-# import pandas as pd
-from utils import apply_dbscan_clustering, load_reduced_features, load_meta_data, constrain_cluster_size
+from utils import load_meta_data
 
-
-# define paths 
-impath = "spectrogram_images"
-path_features = os.path.join("extracted_features/features_reduced_2.npz")
 path_meta_data = os.path.join("metadata/downloaded_data_meta.pkl")
-
 df_meta = load_meta_data(path = path_meta_data)
-
-st.divider()
-
+st.header("xeno-canto data credits, licences and details")
+st.page_link("https://xeno-canto.org/", label=":gray[Link to xeno-canto web]")
 st.dataframe(df_meta)
-
-st.dataframe(df_meta['lic'].value_counts())
+# st.dataframe(df_meta['lic'].value_counts())
+st.divider()
--- stmain.py
+++ stmain.py
@@ -16,45 +16,41 @@ if 'colors_2' not in ss:
 st.set_page_config(layout="wide")
 
 p01 = st.Page("st_page_01.py", title="Interactive")
-p02 = st.Page("st_page_02.py", title="Data details")
+p02 = st.Page("st_page_02.py", title="Data detes and licences")
+p03 = st.Page("st_page_03.py", title="Methods")
 
-pg = st.navigation([p01, p02])
+pg = st.navigation([p01, p02, p03])
 
 pg.run()
 
-
-
-
-
-
-
-
 with st.sidebar:
 
-    # st.markdown(''':red[#**Clustering unselected bio-acoustic data**]''')
-    st.header(''':red[**Clustering unselected bio-acoustic data**]''')
-
-    st.markdown(''':red[**-- Under development --**]''')
-
+    st.header(''':red[**CLUSTERING UNSELECTED BIO-ACOUSTIC RECORDINGS**]''')
+    st.markdown(''':red[**Preliminary and still under development**]''')
     st.markdown(''' 
-        Explore spectrogram clusters obtained via auto-encoders, dimensionality reduction via UMAP, and clustering with DBSCAN.
-        This page only features the last step, exploration of clustering with DBSCAN. Feature extraction (auto-encoders) and dimensionality reduction (UMAP) is currently performed offline and still heavily experimental.                          
-        
-        Currently based on a static dataset of 60560 grey-scale mini-images of 128x128 pixels.
-        Images are MP3 recording downloaded from xeno-canto and transformed to spectrograms.
-        
+        **SUMMARY:**
+        Explore spectrogram clusters obtained via auto-encoders, dimensionality reduction and unsupervised clustering.
+        This page currently only features clustering with DBSCAN.
+        Feature extraction with auto-encoders and dimensionality reduction with UMAP is currently performed offline and still experimental. 
+         
+        **CHALLENGE:**
+        The training and test images are unselected: the audio files were sequentially cut into pieces, thus
+        many clips contain only nuisance or background noise and calls are often truncated.
+        All XC-recorings have mp3 artifacts and some have high/low pass filtering artifacts. 
+                
+        **LONG-TERM OBJECTIVE:**
+        Once ready, this tool could contribute to automatically create pre-labelled dataset with a fine time-scale.
+        This could considerably speed-up the the manually confirmed annotation of a huge dataset taken from XC to train supervised models.                   
         ''')
-
     
-
-
-
-
-    # st.title(""); st.title(""); st.title(""); st.title(""); st.title(""); st.title(""); st.title("")
-    st.title(""); st.title(""); st.title(""); st.title("") 
-    st.markdown(''':gray[Acoutic data from xeno-canto pre-processed with ]''')
-    st.page_link("https://github.com/sergezaugg/xeno_canto_organizer", label=":gray[xeno-canto organizer]")
+    # st.title(""); st.title("")
+    st.markdown(''':red[LINKS]''')
     st.page_link("https://xeno-canto.org/", label=":gray[xeno-canto]")
+    st.page_link("https://github.com/sergezaugg/xeno_canto_organizer", label=":gray[xeno-canto organizer]")
+
+
+
+
 
 
 
